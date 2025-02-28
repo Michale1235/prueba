6d441af55919e8fd83ffb3dc643dc22528e26f81
Michael <michaelandresandres7514@gmail.com>
2025-02-28 17:00:29 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MiningApp from "./app";
+import "./style.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<MiningApp />);
