45dcee11774f751223392f218592797edbc66f21
Michael <michaelandresandres7514@gmail.com>
2025-02-28 17:02:53 -0500

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MiningApp from "./app";
-import "./style.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<MiningApp />);
